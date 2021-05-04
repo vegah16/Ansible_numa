0c1e8e47f52f18c1caef5346d14361546b42122c
Roar Førde <roarforde@gmail.com>
2021-05-04 13:36:26 +0200

    test results

--- math.py
+++ math.py
@@ -1,7 +1,10 @@
+import sys
+
 def percent(num1, num2):
     return (num1 / num2)
 # Take input from the user
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-print("The percent is", percent(num1, num2)*100)
+with open('/home/rof/Ansible_numa/results/percentage.txt', 'w') as f:
+    print("The packetdrop percentage is", percent(num1, num2)*100, file=f)
