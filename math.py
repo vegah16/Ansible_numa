4cea375e915ec4d86fc2f578823dec441befe38c
Roar Førde <roarforde@gmail.com>
2021-05-04 10:52:26 +0200

    fiksa eit par småfeil og lagde ein enkel kalkulator

---
+++ math.py
@@ -0,0 +1,7 @@
+def percent(num1, num2):
+    return (num1 / num2)
+# Take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("The percent is", percent(num1, num2)*100)
